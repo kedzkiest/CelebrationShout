0f36a30d379b84aa3620b936a5da35fe2d2a2f59
kedzkiest <kaede12kazu22@gmail.com>
2023-12-21 13:31:11 +0900

    add background for ingame

--- CelebrationShout/Assets/Scripts/UI/InGameUI.cs
+++ CelebrationShout/Assets/Scripts/UI/InGameUI.cs
@@ -1,18 +1,16 @@
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InGameUI : MonoBehaviour
 {
+    /// <summary>
+    /// The background image of title.
+    /// </summary>
     [SerializeField]
-    private TextMeshProUGUI text;
-
-    public void Initialize()
-    {
-
-    }
+    private Image background;
 
     public void SetVisible(bool _isVisible)
     {
-        text.enabled = _isVisible;
+        background.enabled = _isVisible;
     }
 }
