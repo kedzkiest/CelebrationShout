c46076310cdcbc17ff93218004832a5e4bfd0fbe
kedzkiest <kaede12kazu22@gmail.com>
2023-12-21 09:55:20 +0900

    add background to title

--- CelebrationShout/Assets/Scripts/UI/TitleUI.cs
+++ CelebrationShout/Assets/Scripts/UI/TitleUI.cs
@@ -1,8 +1,21 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleUI : MonoBehaviour
 {
+    /// <summary>
+    /// The background image of title.
+    /// </summary>
+    [SerializeField]
+    private Image background;
+
+    /// <summary>
+    /// The panel to separate texts and background.
+    /// </summary>
+    [SerializeField]
+    private Image panel;
+
     /// <summary>
     /// The title of the game.
     /// </summary>
@@ -35,6 +48,8 @@ public class TitleUI : MonoBehaviour
 
     public void SetVisible(bool _isVisible)
     {
+        background.enabled = _isVisible;
+        panel.enabled = _isVisible;
         title.enabled = _isVisible;
         instruction.SetVisible(_isVisible);
         guideToNext.enabled = _isVisible;
